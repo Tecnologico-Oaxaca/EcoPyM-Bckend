2647303cbbbc80562a4539436e9e5d76a11bdeaa
SoyFroylanMtz <soyfroylanmtzcarlos@gmail.com>
2024-05-17 13:32:29 -0600

    Desarrollo de Api Busine

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\BusineController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/busine',[BusineController::class, 'index']);
+Route::get('/busine/{id}',[BusineController::class, 'show']);
+Route::post('/busine',[BusineController::class, 'store']);
+Route::put('/busine/{id}',[BusineController::class, 'update']);
+Route::patch('/busine/{id}',[BusineController::class, 'updatePartial']);
+Route::delete('/busine/{id}',[BusineController::class, 'destroy']);
