62e1ecc10c45f95ba9705f06b017b6738c7626a2
SoyFroylanMtz <soyfroylanmtzcarlos@gmail.com>
2024-05-19 10:48:53 -0600

    Desarrollo Api Area

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AreaController;
 use App\Http\Controllers\BranchController;
 use App\Http\Controllers\BusineController;
 use App\Http\Controllers\MipymeController;
@@ -31,3 +32,10 @@ Route::post('/branches',[BranchController::class, 'store']);
 Route::put('/branches/{id}',[BranchController::class, 'update']);
 Route::patch('/branches/{id}',[BranchController::class, 'updatePartial']);
 Route::delete('/branches/{id}',[BranchController::class, 'destroy']);
+
+Route::get('/areas',[AreaController::class, 'index']);
+Route::get('/areas/{id}',[AreaController::class, 'show']);
+Route::post('/areas',[AreaController::class, 'store']);
+Route::put('/areas/{id}',[AreaController::class, 'update']);
+Route::patch('/areas/{id}',[AreaController::class, 'updatePartial']);
+Route::delete('/areas/{id}',[AreaController::class, 'destroy']);
