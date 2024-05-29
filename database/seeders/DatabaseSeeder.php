96a7ff6ba2edb5cfd27ca429e1463bce88899a5c
davidrolito7 <139143108+davidrolito7@users.noreply.github.com>
2024-05-28 18:07:58 -0600

    actualizar DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,72 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
+     /**
      * Seed the application's database.
      */
     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $turnos = [
+            'Turno completo mañana',
+            'Turno completo tarde',
+            'Medio turno tarde',
+            'Medio turno mañana'
+        ];
+
+        foreach ($turnos as $turno) {
+            WorkShift::create([
+                'name' => $turno,
+            ]);
+        }
+
+        $days = [
+            'Lunes',
+            'Martes',
+            'Miércoles',
+            'Jueves',
+            'Viernes',
+            'Sábado',
+            'Domingo'
+        ];
+
+        foreach ($days as $day) {
+            Day::create([
+                'name' => $day,
+            ]);
+        }
+
+        $areas = [
+            'Almacén',
+            'Ventas',
+            'Proveedores',
+            'Registro',
+        ];
+
+        $roles = [
+            ['name' => 'Gerente', 'guard_name' => 'Supervisor'],
+            ['name' => 'Cajero', 'guard_name' => 'Cajero'],
+            ['name' => 'Almacenista', 'guard_name' => 'Almacen'],
+        ];
+
+        foreach ($areas as $areaName) {
+            $area = Area::create([
+                'name' => $areaName,
+            ]);
+
+            // Asociar roles a esta área
+            foreach ($roles as $roleData) {
+                $existingRole = Role::where('name', $roleData['name'])->where('guard_name', $roleData['guard_name'])->first();
+
+                if (!$existingRole) {
+                    $role = Role::create($roleData);
+                } else {
+                    $role = $existingRole;
+                }
+
+                $area->roles()->attach($role);
+            }
+        }
     }
 }
